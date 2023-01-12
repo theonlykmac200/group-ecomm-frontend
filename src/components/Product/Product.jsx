6e33fe3acc49fa1e573a7ea57014ce654f93d5b0
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-01-11 21:31:41 -0800

    products data added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,21 @@
+import React from "react";
 import "./Product.css";
 
-const Product = () => {
-  return <div>Product</div>;
+function Product ({name, description, price, quantity}) {
+  return <div className="product">
+    <p>Product name</p>
+    <div className="product_desc">
+      <p>Product description</p>
+      <div className="product_price">
+        <p>Product price</p>
+      <div className = "product_quant"></div>
+      <p>Product quantity</p>
+      </div>
+    </div>
+  </div>;
+
+ 
+  <button>Add to basket</button>
 };
 
 export default Product;
-
-// testing 
