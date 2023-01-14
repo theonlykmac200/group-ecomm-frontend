91f3cac7bdf8fe95ca54d1064ff1abc76ed5649e
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-01-13 22:27:04 -0800

    product displayed show page

--- src/pages/Show/Show.jsx
+++ src/pages/Show/Show.jsx
@@ -1,12 +1,22 @@
 import "./Show.css";
-import Product from "../../components/Product/Product";
+import { useNavigate, useParams } from "react-router";
 
-function Show () {
+function Show ({products}) {
+
+  const { id } = useParams();
+  const product = products.find((product) => product._id === id);
+  const navigate = useNavigate();
+
+
   return (
-    <div className="show">Show page
+    <div className="show">
+      <h1>{product.Title}</h1>
+      <h1>{product.desc}</h1>
+      <h1>{product.Price}</h1>
+      <img src={product.img} alt={product.Title} />
+      <h1>{product._id}</h1>
     </div>
   )
 };
 
 export default Show;
-
