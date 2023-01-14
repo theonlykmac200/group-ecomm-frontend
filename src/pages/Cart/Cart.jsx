a8ecdd4785fbd9a9f506e1f00ebfc2b99ae51547
cps97 <colinstoutt@gmail.com>
2023-01-13 20:03:52 -0800

    add qty

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -9,6 +9,72 @@ const Cart = () => {
         <div className="cart__item">
           <div className="cart__item-img"></div>
           <p className="cart__item-title">Product Title</p>
+          <p className="cart__item-qty">
+            Qty:
+            <select name="qty">
+              <option default value="1">
+                1
+              </option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </p>
+
+          <button className="delete">Delete</button>
+          <p>$29.99</p>
+        </div>
+        <div className="cart__item">
+          <div className="cart__item-img"></div>
+          <p className="cart__item-title">Product Title</p>
+          <p className="cart__item-qty">
+            Qty:
+            <select name="qty">
+              <option default value="1">
+                1
+              </option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </p>
+
+          <button className="delete">Delete</button>
+          <p>$29.99</p>
+        </div>
+        <div className="cart__item">
+          <div className="cart__item-img"></div>
+          <p className="cart__item-title">Product Title</p>
+          <p className="cart__item-qty">
+            Qty:
+            <select name="qty">
+              <option default value="1">
+                1
+              </option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </p>
+
           <button className="delete">Delete</button>
           <p>$29.99</p>
         </div>
