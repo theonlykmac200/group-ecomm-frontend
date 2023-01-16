5cc0abe4b462cff654e165283e68b055da7129d0
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-01-16 14:38:20 -0800

    product rating icons added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
+import StarIcon from '@mui/icons-material/Star';
 
 function Product({ name, description, price, quantity, id, img }) {
   return (
@@ -8,6 +9,12 @@ function Product({ name, description, price, quantity, id, img }) {
       <Link className="link" to={`/product/${id}`}>
         <p className="product_name">{name.slice(0, 70) + "..."}</p>
         <p className="product_price">${price}.99</p>
+        <div className="rating">
+        <StarIcon id="product_rating"/> 
+        <StarIcon id="product_rating"/>
+        <StarIcon id="product_rating"/>
+        <StarIcon id="product_rating"/>
+        </div>
         <p className="product_quant">{quantity}</p>
         <img className="product_img" src={img} alt={name} />
       </Link>
