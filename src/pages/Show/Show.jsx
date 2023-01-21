dbdb36ae1d4526366d719d3231f304b8b43f9850
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-01-21 00:55:12 -0800

    show page styling complete

--- src/pages/Show/Show.jsx
+++ src/pages/Show/Show.jsx
@@ -1,6 +1,6 @@
 import "./Show.css";
 import { useParams } from "react-router";
-import StarIcon from '@mui/icons-material/Star';
+
 
 function Show({ products }) {
 
@@ -19,12 +19,9 @@ function Show({ products }) {
 
         <div className="center">
           <h1 className="product-title">{product.Title}</h1>
-          <StarIcon id="star-rating"/> 
-          <StarIcon id="star-rating"/> 
-          <StarIcon id="star-rating"/> 
-          <StarIcon id="star-rating"/> 
+         
 
-          <p className="product-price">${product.Price}</p>
+          <p className="product-price">Price: ${product.Price}</p>
 
           <div className="paragraph-two">
           <strong className="about-item">About this item: </strong> 
@@ -33,16 +30,18 @@ function Show({ products }) {
         </div>
 
         <div className="right">
-          {/* <div className="section"> */}
+          
           <p className="free-returns">FREE Returns</p>
           <p className="free-delivery">FREE delivery January 26-30.</p>
           <p className="order-soon">Only 1 left in stock - order soon</p>
-          <button className="buy-button">Buy now</button>
+
+          
+        
           <button className="add-cart">Add to cart</button>
           </div>
         </div>
       </div>
-    // </div>
+   
 
 
 
