697efbde8487a31390d9a25387a7b245e73026e4
cps97 <colinstoutt@gmail.com>
2023-01-20 16:23:57 -0800

    addToCart function

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
-import StarIcon from '@mui/icons-material/Star';
+import StarIcon from "@mui/icons-material/Star";
 
-function Product({ name, description, price, quantity, id, img }) {
+function Product({ product, name, addToCart, price, quantity, id, img }) {
   return (
     <div className="product">
       <Link className="link" to={`/product/${id}`}>
         <p className="product_name">{name.slice(0, 70) + "..."}</p>
-        <p className="product_price">${price}.99</p>
+        <p className="product_price">${price}</p>
         <div className="rating">
-        <StarIcon id="product_rating"/> 
-        <StarIcon id="product_rating"/>
-        <StarIcon id="product_rating"/>
-        <StarIcon id="product_rating"/>
+          <StarIcon id="product_rating" />
+          <StarIcon id="product_rating" />
+          <StarIcon id="product_rating" />
+          <StarIcon id="product_rating" />
         </div>
         <p className="product_quant">{quantity}</p>
         <img className="product_img" src={img} alt={name} />
       </Link>
-      <button>Add to cart</button>
+      <button onClick={() => addToCart(product)}>Add to cart</button>
     </div>
   );
 }
