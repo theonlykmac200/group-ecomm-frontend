ae0abd1471ddde7000eadca973f86c8ece77a3d2
cps97 <colinstoutt@gmail.com>
2023-01-20 18:31:59 -0800

    display cart items

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -12,19 +12,25 @@ const Cart = () => {
     <div className="cart">
       <div className="cart__container">
         <h1 className="cart__title">Shopping Cart</h1>
-        <div className="cart__item">
-          <div className="cart__item-img"></div>
-          <p className="cart__item-title">Product Title</p>
-          <div className="cart__item-qty">
-            Qty:
-            <span>-</span>
-            <p>1</p>
-            <span>+</span>
-          </div>
+        {cart.map((product) => {
+          return (
+            <div className="cart__item">
+              <img src={product.img} alt={product.Title} />
+              <p className="cart__item-title">
+                {product.Title.slice(0, 30) + "..."}
+              </p>
+              <div className="cart__item-qty">
+                Qty:
+                <span>-</span>
+                <p>{product.qty}</p>
+                <span>+</span>
+              </div>
 
-          <button id="delete">Delete</button>
-          <p>$29.99</p>
-        </div>
+              <button id="delete">Delete</button>
+              <p>{product.Price}</p>
+            </div>
+          );
+        })}
       </div>
       <aside className="subtotal">
         <h1>Subtotal (3 items)</h1>
